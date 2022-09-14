5cf346e91ba4aea5c06a9764dd69f7797981680c
fif <fif@DESKTOP-BFHUIKE>
2022-09-15 06:52:54 +1200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Скрипников варинт 17
+
+Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+double N = 3 + Math.Pow(y, 2) + Math.Sqrt(y + 1);
+Console.WriteLine($"N = {N:F2}");
